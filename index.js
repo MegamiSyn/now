6ff13cbc1fc13d11931d995a17451fdab9c9bb74
Syned <megamisyn@gmail.com>
2022-12-04 21:33:54 -0300

    A

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 
@@ -8,5 +9,11 @@ app.get('/api/', (req, res) => {
 app.all('*', (req, res) => {
   res.status(404).send({ message: 'route not defined!' });
 });
+
+const DB_USER = process.env.DB_USER
+const DB_PASS = encodeURIComponent(process.env.DB_PASS)
+
+console.log(DB_PASS);
+console.log(DB_USER);
 
 module.exports = app;
