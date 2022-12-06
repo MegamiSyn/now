19862fa5f2804c5b2a7e52cdae5768c462f9f46e
Syned <megamisyn@gmail.com>
2022-12-06 05:20:33 -0300

    2

--- models/Movie.js
+++ models/Movie.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const Movie = mongoose.model('Movie',{
-    name: String,
-    url_m: String,
-    img_url: String,
+const movieSchema = new Schema({
+    name: { type: String, required: true },
+    url_m: { type: String, unique: true, required: true },
+    img_url: { type: String, required: true },
     versionKey: false
-})
+});
 
-module.exports = Movie
+module.exports = Movie = mongoose.model('movie', movieSchema);
